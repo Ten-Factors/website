5e2f6d580c85357c8cf3e59d41b86141113f6837
Bohdan Tron <bohdan.tron@gmail.com>
2025-08-21 18:58:24 +0300

    fix: read further distance

--- ten-factors-new/src/components/ui/ReadFurtherCard.jsx
+++ ten-factors-new/src/components/ui/ReadFurtherCard.jsx
@@ -4,7 +4,7 @@ export default function ReadFurtherCard({ title, desc, href = '#' }) {
   return (
     <a
       href={href}
-      className="bg-white flex flex-col items-center justify-start overflow-clip rounded-2xl primary-shadow w-full max-w-[630px] focus:outline-none focus:ring-2 focus:ring-yellow-400"
+      className="bg-white flex flex-col items-center justify-start overflow-clip rounded-2xl primary-shadow w-full focus:outline-none focus:ring-2 focus:ring-yellow-400"
     >
       <div className="flex flex-col gap-4 items-start justify-start px-10 py-8 w-full">
         <div className="flex flex-col gap-3 items-start text-left text-primary w-full">
